700013dcc51954899f7c33f465ca0a2a24199dd0
deropage <deropage@gmail.com>
2023-01-09 21:18:14 -0600

    First Stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,28 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your magic number? ");
-        string tempMagicNumber = Console.ReadLine();
-        int magicNumber = int.Parse(tempMagicNumber);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0,1000);
+        int count = 0;
         int? guessNumber = null;
 
         while ( magicNumber != guessNumber)
         {
+            count += 1;
             Console.Write("What is your guess? ");
             string tempGuessNumber = Console.ReadLine();
             guessNumber = int.Parse(tempGuessNumber);
 
             if (magicNumber == guessNumber)
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed it after {count} tries.");
             }
             else if (magicNumber < guessNumber)
             {
-                Console.WriteLine("Lower");
+                Console.WriteLine($"Lower \nYou have tried {count} times.");
             }
             else 
             {
-                Console.WriteLine("Higher");
+                Console.WriteLine($"Higher \nYou have tried {count} times.");
             }
         }
             
