85128ae5934eb5adbf31146ec70a894662f4c977
deropage <deropage@gmail.com>
2023-01-09 21:25:48 -0600

    Complete Stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,32 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(0,1000);
-        int count = 0;
-        int? guessNumber = null;
-
-        while ( magicNumber != guessNumber)
+        string tryAgain = "Yes";
+        do
         {
-            count += 1;
-            Console.Write("What is your guess? ");
-            string tempGuessNumber = Console.ReadLine();
-            guessNumber = int.Parse(tempGuessNumber);
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(0,1000);
+            int count = 0;
+            int? guessNumber = null;
+            while ( magicNumber != guessNumber)
+            {
+                count += 1;
+                Console.Write("What is your guess? ");
+                string tempGuessNumber = Console.ReadLine();
+                guessNumber = int.Parse(tempGuessNumber);
+                if (magicNumber == guessNumber)
+                {
+                    Console.WriteLine($"You guessed it after {count} tries.");
+                    Console.WriteLine("Do you want to try again?");
+                    tryAgain = Console.ReadLine();
+                
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine($"Lower \nYou have tried {count} times.");
+                }
+                else 
+                {
+                    Console.WriteLine($"Higher \nYou have tried {count} times.");
+                }
+            }
+        }while (tryAgain == "Yes");
 
-            if (magicNumber == guessNumber)
-            {
-                Console.WriteLine($"You guessed it after {count} tries.");
-            }
-            else if (magicNumber < guessNumber)
-            {
-                Console.WriteLine($"Lower \nYou have tried {count} times.");
-            }
-            else 
-            {
-                Console.WriteLine($"Higher \nYou have tried {count} times.");
-            }
-        }
             
-
     }
 }
