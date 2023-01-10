1392caeb6a9ec7d40a2936f4e345da2f5a8151df
deropage <deropage@gmail.com>
2023-01-09 22:22:44 -0600

    Full Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -10,6 +10,7 @@ class Program
         int listSum=0;
         float listAvg=0;
         int listMax=0;
+        int listMinPositive = 0;
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         do 
         {
@@ -23,20 +24,32 @@ class Program
             
         }while (tempNumberInt != 0);
 
+        listMinPositive = numbers[0];
+
         foreach (int number in numbers)
         {
             listSum = listSum + number;
             if (number > listMax)
             {
                 listMax = number;
+            }      
+            if (number > 0 && number < listMinPositive)
+            {
+                listMinPositive = number;
             }
-            
         }
+        numbers.Sort();
 
         listAvg = (float)listSum / (float)numbers.Count;
         Console.WriteLine($"The Sum is: {listSum}");
         Console.WriteLine($"The Average is: {listAvg}");
         Console.WriteLine($"The largest number is: {listMax}");
+        Console.WriteLine($"The smallest positive number is: {listMinPositive}");
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
 
         
     }
