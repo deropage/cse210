3e41f460629578530bf79367bf6906236a745db6
deropage <deropage@gmail.com>
2023-02-12 00:50:36 -0600

    Any lenght of scripture

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -7,24 +7,35 @@ class Scripture
     private List<string> _words = new List<string>();
     private Word _word = new Word();
     private string _content = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
-    private string answer = "";
-    private Random rand = new Random();
+    private string _answer = "";
+    private Random _rand = new Random();
+    private int _index = 0;
+    private int _counter = 0; 
+    private int _changes = 0; 
     private string[] words = new string[] {};
+
     private List<int> _trackIndex = new List<int>();
+    
   
 
     public Scripture(Reference reference)
     {
         do
         {
+            
             Console.Write(reference.GetReference());
-            ShowScripture();
+            ShowScripture();     
             Console.WriteLine("Press enter to continue or type 'quit' to finish:");
-            answer = Console.ReadLine();
+            
+            _answer = Console.ReadLine();
+            if(_changes == _words.Count && _words.Count > 0){_answer="quit";}
+            
             HideWords();
+            
+  
 
 
-        } while (answer != "quit");
+        } while (_answer != "quit");
     }
 
     private void HideWords()
@@ -32,21 +43,23 @@ class Scripture
         _words.Clear();
         words = _content.Split(" ");
         foreach (string word in words){_words.Add(word);}
-
-        for (int i = 0; i < 3 ; i++)
+        _counter = 0;
+        while(_counter < 3 && _changes < _words.Count)
         {
-            int _index = rand.Next(_words.Count);
-            if (!_trackIndex.Contains(_index))
+            _index = _rand.Next(_words.Count); 
+            if(_words[_index].Contains("_"))
+            {
+                _index = _rand.Next(_words.Count);
+            }
+            else
             {
                 _word = new Word(_words[_index]);
                 _words[_index] = _word.GetWord();
-                _trackIndex.Add(_index);
-                Console.WriteLine(_trackIndex.Count);
-                Console.WriteLine(_words.Count);
-            } 
-            else if (_trackIndex.Count < _words.Count-1) {i = i - 1;}
-            
-        } 
+                _counter++;
+                _changes++;
+            }
+            //Console.Write(_changes);
+        }
 
         _content = String.Join(" ", _words);
     }
