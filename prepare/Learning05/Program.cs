a7d2261e08782b34fbd2683e0e416722af53a84b
deropage <deropage@gmail.com>
2023-02-28 19:27:59 -0600

    completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,15 +2,21 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.Clear();
+        List<Shape> _myShapeList = new List<Shape>();
         Square _mySquare = new Square("Red", 20);
         Circle _myCircle = new Circle("Blue", 15);
         Rectangle _myRectangle = new Rectangle("Green", 15, 20);
-
-        _mySquare.GetShapeInfo();
-        _myCircle.GetShapeInfo();
-        _myRectangle.GetShapeInfo();
+        _myShapeList.Add(_mySquare);
+        _myShapeList.Add(_myCircle);
+        _myShapeList.Add(_myRectangle);
+        
+        Console.Clear();
+        foreach (Shape shape in _myShapeList)
+        {
+            shape.GetShapeInfo();
+        }
     }
 }
